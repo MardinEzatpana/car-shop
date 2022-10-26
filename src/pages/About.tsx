1bb3668708218dd7177f63875d292a315d9e9448
Mardin <mardin.ezatpana@gmail.com>
2022-10-26 11:39:50 +0330

    add home and aboutPage to project

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,27 @@
-
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h1>About</h1>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h3>Lorem ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              totam, ratione, voluptas eveniet soluta incidunt itaque optio
+              beatae id temporibus, ex exercitationem sint ad earum in sed?
+              Quam, cum sunt.
+            </p>
+            <Link className="btn btn-dark" to="/store">
+              Store
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <h1>Home</h1>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+          <h3>Lorem ipsum</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              totam, ratione, voluptas eveniet soluta incidunt itaque optio
+              beatae id temporibus, ex exercitationem sint ad earum in sed?
+              Quam, cum sunt.
+              <br />
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+              totam, ratione, voluptas eveniet soluta incidunt itaque optio
+              beatae id temporibus, ex exercitationem sint ad earum in sed?
+              Quam, cum sunt.
+            </p>
+            <Link className="btn btn-dark" to="/store">
+              Store
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
